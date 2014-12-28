cf797d6bcffe393b5ca48e33bb9b9b5c1e6cce66
James Kiesel <james.kiesel@gmail.com>
2014-12-28 22:25:18 +1300

    Using Struct (**** yeah). Coming again to save the mfingdayyeah.

--- app/models/search_result.rb
+++ app/models/search_result.rb
@@ -1,23 +1,3 @@
-class SearchResult
-  include ActiveModel::Serialization
-
-  attr_accessor :result
-  def initialize(result, query, priority)
-    @result = result
-    @priority = priority
-    @query = query
-  end
-
-  def read_attribute_for_serialization(field)
-    attributes[field]
-  end
-
-  def attributes
-    {
-      query: @query,
-      result: @result,
-      priority: @priority
-    }
-  end
-
+SearchResult = Struct.new(:result, :query, :priority) do
+  alias :read_attribute_for_serialization :send
 end
